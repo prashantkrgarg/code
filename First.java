b4e13de0532494e6eaab8416b23448dc6709053d
Prashant kumar <prashantkumar0w7@gmail.com>
2024-10-13 21:05:29 +0530

    git first

---
+++ First.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.print("prashant");
+        Scanner sc = new  Scanner(System.in);
+        int valu = sc.nextInt();
+        System.out.println(valu);
+        sc.close();
+
+    }
+}
